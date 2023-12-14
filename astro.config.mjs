9811b312b624668935831e822d4c386953becd82
Manuel Digregorio <manueldigregorio@Manuels-MacBook-Air.local>
2023-12-14 15:04:31 -0300

    reconfig of deploy.yml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.goodneys.com",
+  //base: "/goodneys-landing",
   integrations: [tailwind(), react()],
   vite: {
     ssr: {
